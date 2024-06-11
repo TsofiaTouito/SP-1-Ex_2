04c4b126ed9ec882786874ff01d41836c5c51365
TsofiaTouito <148336183+TsofiaTouito@users.noreply.github.com>
2024-06-11 16:29:52 +0300

    Create my_mat.h

---
+++ my_mat.h
@@ -0,0 +1,22 @@
+#ifndef _MY_MAT_H
+#define _MY_MAT_H
+
+#define SIZE 10
+#define TRUE 1
+#define FALSE 0
+#define INFINITY 2147483647 // The max number of int
+#define MAX_ITEM 5
+#define MAX_WEIGHT 20
+#define MAX_S_LEN 20
+
+// This functions is getting the values for the matrix from the user
+// Getting a pointer to the start of the matrix, and int valuse for the size of it
+void getMatrixValues(int[SIZE][SIZE], int);
+
+int isPath(int[SIZE][SIZE], int,int, int);
+
+int shortestPath(int[SIZE][SIZE], int, int, int);
+
+int knapsack( int[MAX_ITEM], int[MAX_ITEM], int[MAX_ITEM]);
+
+#endif
